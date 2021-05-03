2c77cde0c42baf60d7c9f7bcbf788d8dd3230e6c
ATRi111 <1657125180@qq.com>
2021-05-03 15:48:40 +0800

    new

---
+++ Assets/Scripts/Map/Cloud/Cloud_Trigger.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cloud_Trigger : MonoBehaviour
+{
+    private void OnCollisionEnter2D(Collision2D other) 
+    {
+        Public.IPlayer player = other.gameObject.GetComponent<Public.IPlayer>();
+        if (player != null)
+        {
+            var t = GetComponentsInChildren<CSting_Movable>();
+            foreach(CSting_Movable item in t)
+            {
+                item.StartMove();
+            }
+        }    
+    }
+}
---
+++ Assets/Scripts/System/CSceneManager.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using UnityEngine;
+using Public;
+using UnityEngine.UI;
+using static UnityEngine.SceneManagement.SceneManager;
+
+public class CSceneManager : Sigleton<CSceneManager>
+{
+    //当前关的index
+    [SerializeField] private int _Index;
+    internal int Index
+    {
+        get
+        {
+            return _Index;
+        }
+        set
+        {
+            _Index = value;
+            LoadScene(value);
+            CEventSystem.Instance.SceneLoaded(value);
+        }
+    }
+
+    [SerializeField] private Pre_LoadScene loadScenePrefab;
+
+    public void LoadNextLevelImmediately()
+    {
+        Index++;
+    }
+    public void LoadNextLevel()
+    {
+        StartCoroutine(ILoadLevel(Index + 1));
+    }
+    IEnumerator ILoadLevel(int index)
+    {
+        loadScenePrefab.gameObject.SetActive(true);
+        AsyncOperation asyncLoad = LoadSceneAsync(index);
+        asyncLoad.allowSceneActivation = false;
+        for (; !asyncLoad.isDone;)
+        {
+            loadScenePrefab.slider.value = asyncLoad.progress;
+            if (asyncLoad.progress >= 0.9f)
+            {
+                loadScenePrefab.text.gameObject.SetActive(true);
+                loadScenePrefab.slider.value = 1.0f;
+                if (Input.anyKeyDown)
+                {
+                    loadScenePrefab.gameObject.SetActive(false);
+                    asyncLoad.allowSceneActivation = true;
+                }
+            }
+            yield return null;
+        }
+        Index = index;
+    }
+    public void Exit()
+    {
+        Index = 0;
+    }
+}
