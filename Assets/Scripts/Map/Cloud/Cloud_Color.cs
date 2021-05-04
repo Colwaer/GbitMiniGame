032b044f81c0c238b5f9d16853de9775dcd68667
Colwaer <674329253@qq.com>
2021-05-04 16:45:54 +0800

    wind zone

---
+++ Assets/Scripts/Map/Cloud/Cloud_Color.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+public class Cloud_Color : MonoBehaviour
+{
+    private Tilemap tilemap;
+    public WindArea windArea;
+    public Color targetColor;
+    public float ColorChangeTime = 1.5f;
+    private bool isEnabled = true;
+    private void Awake() 
+    {
+        tilemap = GetComponent<Tilemap>();    
+    }
+    private void OnCollisionEnter2D(Collision2D other) 
+    {
+        if (!isEnabled)
+            return;
+        if (other.collider.CompareTag("Player"))
+        {
+            if (other.collider.attachedRigidbody.velocity.magnitude > 13f)
+            {
+                isEnabled = false;
+                windArea.Count++;
+                Debug.Log("start change color");
+                StartCoroutine(ChangeColor(ColorChangeTime));
+            }
+        }    
+    }
+    IEnumerator ChangeColor(float time)
+    {
+        float timer = 0;
+        Color originColor = tilemap.color;
+        while (timer <= time)
+        {
+            timer += Time.deltaTime;
+            tilemap.color = Color.Lerp(originColor, targetColor, timer / time);
+            yield return null;
+        }
+        Destroy(this);
+    }
+
+}
---
+++ Assets/Scripts/Map/WindArea.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WindArea : MonoBehaviour
+{
+    public GameObject wind;
+    private int m_Count = 0;
+    private bool isSimulated = false;
+    public int Count
+    {
+        get
+        {
+            return m_Count;
+        }
+        set
+        {
+            m_Count = value;
+            if (m_Count == Trigger_Count)
+            {
+                wind.SetActive(true);
+                isSimulated = true;
+            }
+        }
+    }
+    public int Trigger_Count = 5;
+
+    private void Awake() 
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D other) 
+    {
+        if (!isSimulated)
+            return;
+        if (other.CompareTag("Player"))
+        {
+            other.attachedRigidbody.gravityScale = -2f;
+        }    
+    }
+    private void OnTriggerStay2D(Collider2D other) 
+    {
+        if (!isSimulated)
+            return;
+        if (other.CompareTag("Player"))
+        {
+            other.attachedRigidbody.gravityScale = -2f;
+        }    
+    }
+    private void OnTriggerExit2D(Collider2D other) 
+    {
+        if (!isSimulated)
+            return;
+        if (other.CompareTag("Player"))
+        {
+            other.attachedRigidbody.gravityScale = 1f;
+        } 
+    }
+}
