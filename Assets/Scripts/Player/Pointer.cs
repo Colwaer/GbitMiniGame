9ea7dfe6f914758c665faa76fe4e87faddd64233
ATRi111 <1657125180@qq.com>
2021-05-09 08:05:13 +0800

    new

---
+++ Assets/Scripts/Player/Pointer.cs
@@ -0,0 +1,19 @@
+﻿using Public;
+using UnityEngine;
+
+public class Pointer : MonoBehaviour
+{
+    private float Distance = 3f ;
+    private Transform PlayerPos;
+
+    private void Start()
+    {
+        PlayerPos = PlayerController.Instance.Player.transform;
+    }
+
+    private void Update()
+    {
+        transform.position = PlayerPos.position - new Vector3(PlayerController.Instance.Direction.x, PlayerController.Instance.Direction.y, 0f).normalized * Distance;
+        transform.rotation = Quaternion.Euler(0f, 0f, 270f - CTool.Direction2Angle(PlayerController.Instance.Direction));
+    }
+}
